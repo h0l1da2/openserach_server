ec511953884f1df12c61e6d8cd7b99b065a90906
h0l1da2 <holiday.k1@icloud.com>
2024-12-20 16:45:50 +0900

    feat(base): basePackage 추가
    holiday

--- src/main/java/me/holiday/opensearch/OpensearchApplication.java
+++ src/main/java/me/holiday/opensearch/OpensearchApplication.java
@@ -3,7 +3,7 @@ package me.holiday.opensearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "me.holiday")
 public class OpensearchApplication {
 
 	public static void main(String[] args) {
