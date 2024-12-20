abc4275b56c8e3d2cfa33722eb6293b9657ff8f7
h0l1da2 <holiday.k1@icloud.com>
2024-12-20 13:30:43 +0900

    First Commit !

---
+++ src/test/java/me/holiday/opensearch/OpensearchApplicationTests.java
@@ -0,0 +1,13 @@
+package me.holiday.opensearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpensearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
